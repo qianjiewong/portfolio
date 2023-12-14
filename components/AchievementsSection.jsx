583368242f0e2e8942253fcff33113abd5a53a6b
qianjiewong <qianjiewong@gmail.com>
2023-12-14 20:47:03 +0800

    first commit

--- components/AchievementsSection.jsx
+++ components/AchievementsSection.jsx
@@ -16,23 +16,21 @@ const achievementsList = [
 
 ];
 const AchievementsSection = () => {
-    return(
-        <div className=" xl:gap-3 sm:py-3 xl:px-12">
+    return (
+        <div className="xl:gap-3 sm:py-3 xl:px-12">
             <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
-            {achievementsList.map((achievement) => {
-                return(
-                    <div 
-                        key={index} className="flex flex-col items-center justify-center mx-4">
-                        <h2 className="text-white text-4xl font-bold">
-                            {achievement.value}
-                        </h2>
-                        <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-                    </div>
-                )
-            }
-            )}
+                {achievementsList.map((achievement, index) => {
+                    return (
+                        <div key={index} className="flex flex-col items-center justify-center mx-4">
+                            <h2 className="text-white text-4xl font-bold">
+                                {achievement.value}
+                            </h2>
+                            <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+                        </div>
+                    );
+                })}
             </div>
-            </div>
-    )
-}
+        </div>
+    );
+};
 export default AchievementsSection;
--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,6 +3,7 @@ import TypeWritter from "typewriter-effect";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import Image from 'next/image';
 
 
 const HeroSection = () => {
@@ -69,10 +70,10 @@ const HeroSection = () => {
             transition={{ duration: 0.5 }}
           >
           <div className="w-[400px] h-[400px] relative z-0">
-            <img
+          <Image
               src="/HeroSection.png"
               alt="hero section"
-              className=" transform rounded-full flex relative -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="transform rounded-full flex relative -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={450}
               height={450}
             />
